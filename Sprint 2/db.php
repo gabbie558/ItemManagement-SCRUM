f43ec680ac454bc387541dfb43686235effe72c9
gabbie558 <enriquezgabriela558@gmail.com>
2024-09-26 12:16:28 +0800

    Create a new file for database connection

---
+++ Sprint 2/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'item_management';
+$username = 'root'; // Change to your MySQL username
+$password = ''; // Change to your MySQL password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+?>
