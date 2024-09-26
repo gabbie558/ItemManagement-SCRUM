2caf4251935ce500cc8b61b6e34dd475a6e6b0fa
gabbie558 <enriquezgabriela558@gmail.com>
2024-09-26 15:38:35 +0800

    Updated the database connection

--- Sprint 2/db.php
+++ Sprint 2/db.php
@@ -1,13 +1,16 @@
 <?php
-$host = 'localhost';
-$dbname = 'item_management';
-$username = 'root'; // Change to your MySQL username
-$password = ''; // Change to your MySQL password
+$servername = "localhost";
+$username = "root";
+$password = "enriquezzZ@558";
+$dbname = "item_management"; // Make sure this matches the name of the created database
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Could not connect to the database: " . $e->getMessage());
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
+
